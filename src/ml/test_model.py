15f2fe2a5780ccd8bad864ef427545fc5c548d93
Yuxuan <yxzhou61@gmail.com>
2023-07-09 16:38:13 -0400

    updated test code

--- src/ml/test_model.py
+++ src/ml/test_model.py
@@ -1,4 +1,4 @@
-from model import train_model, compute_model_metrics, inference
+from .model import train_model, compute_model_metrics, inference
 import sklearn
 import pickle
 import numpy
@@ -16,12 +16,12 @@ def test_compute_model_metrics():
     y = [1,0,0,1,1]
     preds = [1,1,0,0,0]
     precision, recall, fbeta = compute_model_metrics(y, preds)
-    assert (precision, recall, fbeta) == (0.4, 0.5, 1/3)
+    assert (precision, recall, fbeta) == (0.5, 1/3, 0.4)
 
     y = [0,0,1,1,1]
     preds = [1,1,0,0,0]
     precision, recall, fbeta = compute_model_metrics(y, preds)
-    assert (precision, recall, fbeta) == (1.0, 0.0, 0.0)
+    assert (precision, recall, fbeta) == (0.0, 0.0, 1.0)
 
     y = [1,1,0,0,0]
     preds = [1,1,0,0,0]
@@ -32,6 +32,8 @@ def test_compute_model_metrics():
 def test_inference():
     with open('./data/X_test_sample.pkl', 'rb') as f:
         X_test_sample = pickle.load(f)
-    preds = inference(X_test_sample)
-    assert isinstance([preds, numpy.ndarray])
+    with open('./model/model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    preds = inference(model, X_test_sample)
+    assert isinstance(preds, numpy.ndarray)
     assert set(preds) == {0,1}
